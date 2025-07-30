61ad529f6f090a800a0c5aea7f765ab481529b67
stevanu <ivanstevanu@gmail.com>
2025-07-30 19:25:06 +0700

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 function App() {
   return (
-    <>{/* <h1 className="text-4xl text-red-500 font-bold">Hello Guys</h1> */}</>
+    <>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde culpa
+        repudiandae quas? Eos, velit et? Accusantium ullam quae nobis magnam ex,
+        enim, odio praesentium reprehenderit rem mollitia officia, aut inventore
+        saepe exercitationem eveniet? In sequi nostrum officiis reprehenderit
+        laborum explicabo distinctio laudantium dolores ipsa maxime animi
+        voluptatibus velit perspiciatis aliquam nesciunt culpa, eaque dolor
+        repellendus dolore consequatur obcaecati, cum mollitia expedita! Quae
+        qui laborum esse quas ea vel ab natus consectetur provident rerum,
+        tempore veniam sequi recusandae veritatis porro necessitatibus. Facilis
+        aut placeat, deleniti sequi assumenda nesciunt, mollitia aperiam veniam
+        pariatur velit tempora distinctio labore porro voluptate. Laborum unde
+        architecto libero dolorum asperiores tempore eius saepe, reprehenderit
+        beatae quod cupiditate soluta at dolore debitis sed, modi sit laudantium
+        consequuntur blanditiis inventore. Recusandae earum iste obcaecati
+        explicabo quis, eaque natus odio autem nemo corrupti optio voluptates
+        harum eius quae deleniti qui nam! Illum optio unde, libero non aliquam
+        maxime quos delectus cupiditate consequatur veniam ullam et voluptas
+        tempore dolores sint dolor blanditiis minus itaque recusandae iusto?
+        Molestias odit id iste exercitationem! Ducimus, est nostrum! Error
+        dolorem molestias praesentium animi quam ut. Ipsam itaque totam amet
+        omnis, quidem soluta deserunt at culpa consequatur voluptatum impedit
+        voluptas iste laborum quam nulla sequi obcaecati?
+      </h1>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="navbar py-7 flex items-center justify-between">
       <div className="logo">
@@ -7,26 +26,28 @@ const Navbar = () => {
         </h1>
       </div>
       <ul
-        className="menu flex items-center gap-10 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 
-      -top-10 opacity-0 md:opacity-100 md:top-0"
+        className={`menu flex items-center sm:gap-10 gap-4 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 
+       md:opacity-100 bg-white/32 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl transition-all duration-250 md:bg-transparent ${
+         active ? "top-0 opacity-100" : "-top-15   opacity-100"
+       }`}
       >
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="sm:text-lg text-base font-medium">
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="sm:text-lg text-base font-medium">
             About
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="sm:text-lg text-base font-medium">
             Project
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="sm:text-lg text-base font-medium">
             Contact
           </a>
         </li>
